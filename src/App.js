26d26540a2e3bc153463f846ef6ac30a95286824
sava911 <pesss929@gmail.com>
2023-02-07 22:45:49 +0300

    Project card. Separate component

--- src/App.js
+++ src/App.js
@@ -5,32 +5,19 @@ import './styles/main.css';
 
 
 import NavBar from './compontnts/navBar/NavBar';
-import Header from './compontnts/header/Header';
 import Footer from './compontnts/footer/Footer';
+import Home from './home/Home';
+import Project from './home/Project';
+import Contacts from './home/Contacts';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
 
-      <Header />
-
-      <main className="section">
-        <div className="container">
-
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
-            </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-            </li>
-          </ul>
-
-        </div>
-      </main>
+      {/* <Home /> */}
+      {/* <Project /> */}
+      <Contacts />
       <Footer />
 
 
